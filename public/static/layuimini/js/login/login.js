fdcfc6f091c9790e14a7bcd1eb1dfab33fb4ccd0
conqin <2576109500@qq.com>
2020-09-10 16:52:32 +0800

    更换刷新验证码代码位置

--- public/static/layuimini/js/login/login.js
+++ public/static/layuimini/js/login/login.js
@@ -1,3 +1,9 @@
+//实现点击刷新验证码功能
+$('#refreshCaptcha').on('click', function () {
+    var path = "/office_automation/public/index.php/index/login_controller/sendPageCode";
+    $("#refreshCaptcha").attr('src',path);
+});
+
 //发送登录请求
 function sendData(){
     var userNum  = $("#user-num").val();
