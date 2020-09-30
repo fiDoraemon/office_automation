bb3ca6c40018058d3f4a750ad51e30a23a8d4c51
TZX <18923589045@163.com>
2020-09-30 15:51:25 +0800

    改善了日历上传插件等

--- public/static/layuimini/js/common.js
+++ public/static/layuimini/js/common.js
@@ -24,3 +24,13 @@ function getQueryVariable(variable) {
 
     return false;
 }
+
+// 阻止冒泡
+function stopPropagation(e) {
+    var e = e || window.event;
+    if(e.stopPropagation) { //W3C阻止冒泡方法
+        e.stopPropagation();
+    } else {
+        e.cancelBubble = true; //IE阻止冒泡方法
+    }
+}
