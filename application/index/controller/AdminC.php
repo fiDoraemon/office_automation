fa1e49f24456739280a2315f7f0be0dd9ae9fe21
conqin <2576109500@qq.com>
2020-10-19 15:47:30 +0800

    管理员查看和搜索用户

--- application/index/controller/AdminC.php
+++ application/index/controller/AdminC.php
@@ -12,10 +12,93 @@ use app\common\Result;
 use app\common\util\curlUtil;
 use app\common\util\EncryptionUtil;
 use app\index\model\User;
+use think\db\exception\DataNotFoundException;
+use think\db\exception\ModelNotFoundException;
+use think\exception\DbException;
 use think\Session;
 
 class AdminC
 {
+    /**
+     * 查询所有状态正常的用户
+     * @param int $page
+     * @param int $limit
+     * @return array
+     * @throws \think\Exception
+     */
+//    public function getAllUser($page = 1,$limit = 15){
+//        try {
+//            $user = new User();
+//            $user -> where("user_status",1);
+//            $count =  $user -> count();
+//            $user = new User();
+//            $listUser = $user -> where("user_status",1)
+//                  -> field("user_id,user_name,department_id,phone,email,user_status,create_time,update_time")
+//                  -> page($page,$limit)
+//                  -> order('user_status desc,department_id')
+//                  -> select();
+//            foreach ($listUser as $u){
+//                $u -> department_name = $u -> department -> department_name;
+//                if($u -> user_status === 1){
+//                    $u -> status = "正常";
+//                }else{
+//                    $u -> status = "停用";
+//                }
+//            }
+//            return Result::returnResult(Result::SUCCESS, $listUser,$count);
+//        } catch (DataNotFoundException $e) {
+//        } catch (ModelNotFoundException $e) {
+//        } catch (DbException $e) {
+//        }
+//    }
+
+    /**
+     * 根据用户姓名,部门,状态查询用户
+     * @param string $userName
+     * @param int $departmentId
+     * @param int $userStatus
+     * @param int $page
+     * @param int $limit
+     * @return array
+     * @throws \think\Exception
+     */
+    public function getAllUsers($userName = "",$departmentId = 0,$userStatus = 1,$page = 1,$limit = 15){
+        try {
+            $user = new User();
+            $user -> where("user_status",$userStatus);
+            if($departmentId != 0){
+                $user -> where("department_id",$departmentId);
+            }
+            if($userName != ""){
+                $user ->  where("user_name","like","%$userName%");
+            }
+            $count =  $user -> count();
+            $user -> where("user_status",$userStatus);
+            if($departmentId != 0){
+                $user -> where("department_id",$departmentId);
+            }
+            if($userName != ""){
+                $user ->  where("user_name","like","%$userName%");
+            }
+            $listUser = $user -> field("user_id,user_name,department_id,phone,email,user_status,create_time,update_time")
+                              -> page($page,$limit)
+                              -> order('user_status desc,department_id')
+                              -> select();
+            foreach ($listUser as $u){
+                $u -> department_name = $u -> department -> department_name;
+                if($u -> user_status === 1){
+                    $u -> status = "正常";
+                }else{
+                    $u -> status = "停用";
+                }
+            }
+            return Result::returnResult(Result::SUCCESS, $listUser,$count);
+        } catch (DataNotFoundException $e) {
+        } catch (ModelNotFoundException $e) {
+        } catch (DbException $e) {
+        }
+    }
+
     /**
      * 更新用户钉钉userid
      * @return array
@@ -24,7 +107,6 @@ class AdminC
     {
         // 获取所有用户 userid
         $res = curlUtil::post('http://www.bjzzdr.top/us_service/public/other/ding_ding_c/getAllUserId');
-
         if($res->code == 0) {
             foreach ($res->data as $info) {
                 $user = User::getByUserName($info->name);
@@ -83,5 +165,6 @@ class AdminC
         }
         return false;
     }
+
 
 }
